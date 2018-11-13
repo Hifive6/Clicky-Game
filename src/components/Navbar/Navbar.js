843bc957f3b4b20b7e42eddf5582e4db72144585
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-11-13 18:04:49 -0500

    added navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+//sets up the Navbar Component
+import react, { Component } from "react"
+import "./Navbar.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className = "navbar navbar-default navbar-fixed-top">
+            <ul>
+                <li className = "title">How good is your Memory</li>
+                <li className = "center"></li>
+                <li className = "score">Your Score: {this.props.score}</li>
+            </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
